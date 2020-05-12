3b7aaa6dfaf5686c9307a6f2efc9df786a24c0c8
eliezer droid <eliohernandez94@gmail.com>
2020-05-12 12:11:15 -0600

    initial commit

---
+++ src/main/java/com/voivemailapi/VoiceMailApiApplication.java
@@ -0,0 +1,13 @@
+package com.voivemailapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceMailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceMailApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voivemailapi/VoiceMailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voivemailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceMailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
