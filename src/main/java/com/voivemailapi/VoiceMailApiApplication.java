476b41ecffe95b39cae1348957564d151e02c51e
eliezer droid <eliohernandez94@gmail.com>
2020-05-12 12:17:49 -0600

    first endpoint

--- src/main/java/com/voivemailapi/VoiceMailApiApplication.java
+++ src/main/java/com/voivemailapi/VoiceMailApiApplication.java
@@ -2,12 +2,23 @@ package com.voivemailapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class VoiceMailApiApplication {
-
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(VoiceMailApiApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
---
+++ src/main/java/com/voivemailapi/controller/VoiceMailController.java
@@ -0,0 +1,42 @@
+package com.voivemailapi.controller;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author eliezer
+ *
+ */
+@Controller
+@RestController
+@RequestMapping("/voicemail")
+public class VoiceMailController {
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping("/messages")
+	public String getLista() {
+		String accessToken = "NDY0MmU2NDA0MGNkYjhiODljMzEwYTIxYTA3YzdmNjI6MjMyNjQxNTY1OTA3NWU3NTAwMGNlY2Q3YmNiZjM3NTY=";
+	
+		String url = "https://sandbox.2600hz.com:8443/v2/accounts/4642e64040cdb8b89c310a21a07c7f62/vmboxes/b37675a2d7b90d60f0ee5d4175502394/messages";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.set("Authorization", "Basic "+accessToken);
+		HttpEntity<Object> entity = new HttpEntity<Object>(headers);
+		System.out.println("Entity: "+entity);
+		return restTemplate.exchange(url, HttpMethod.GET, entity, String.class).getBody();
+		
+
+	}
+}
